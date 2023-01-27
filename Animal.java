e7c55f72d05c77127f955f584ea0a4749f438210
almoelmo <121177036+almoelmo@users.noreply.github.com>
2023-01-27 21:52:24 +0500

    interface
    interface

---
+++ Animal.java
@@ -0,0 +1,15 @@
+package kirill.tasks_oop.secondTask;
+
+public class Animal {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
